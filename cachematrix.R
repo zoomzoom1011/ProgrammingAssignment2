03d676ec0637f91680a2a0a8de00bd4d3ce29dba
zoomzoom1011 <35773236+zoomzoom1011@users.noreply.github.com>
2018-11-04 14:18:10 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create a matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-	m <- NULL
+	inv <- NULL
 	set <- function(y) {
 		x <<- y
-		m <<- NULL
+		inv <<- NULL
 	}
 	get <- function() x
-	setmean <- function(mean) m <<- mean
-	getmean <- function() m
+	setinverse <- function(inverse) inv <<- inverse
+	getinverse <- function() inv
 	list(set = set, get = get,
-		 setmean = setmean,
-		 getmean = getmean)
+		 setinverse = setinverse,
+		 getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the mean of the special "vector" 
+## created with the above function. It first checks to see if the mean has already been 
+## calculated. If so, it gets the mean from the cache and skips the computation. 
+## Otherwise, it calculates the mean of the data and sets the value of the 
+## mean in the cache via the setmean function.
 
 cacheSolve <- function(x, ...) {
 	## Return a matrix that is the inverse of 'x'
-	m <- x$getmean()
-	if(!is.null(m)) {
+	inv <- x$getmean()
+	if(!is.null(inv)) {
 		message("getting cached data")
-		return(m)
+		return(inv)
 	}
 	data <- x$get()
-	m <- solve(data, ...)
-	x$setmean(m)
-	m
+	inv <- solve(data, ...)
+	x$setinverse(inv)
+	inv
 }
